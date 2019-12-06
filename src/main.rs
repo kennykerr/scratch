c0dae47500cba522ba5758c3b119a82a08ac9444
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-06 15:09:32 -0800

    structs

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,16 @@ winrt::import!(
         // "windows.ui.composition"
         // "windows.ui.xaml.controls"
         "windows.foundation"
+        "windows.ui"
 );
 
 fn main() -> winrt::Result<()> {
     use windows::ui::*;
     let color = Colors::alice_blue()?;
 
-    assert!(color.a == 255);
-    assert!(color.r == 240);
-    assert!(color.g == 248);
-    assert!(color.b == 255);
+    let s = format!("{:?}", color);
+    assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
     println!("woot!");
-
     Ok(())
 }
