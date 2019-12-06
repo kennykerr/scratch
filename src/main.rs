e245d967f4e300e6de888eaf79022f9a2ce7139a
Kenny Kerr <kekerr@microsoft.com>
2019-12-06 09:07:41 -0800

    files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 winrt::import!(
     dependencies
-        "../folder/containing/winmd/files"
-        "../path/to/specific.winmd"
-        "10.0" // alias for latest Win10 SDK
-        "10.0.19038.0" // alias for specific Win10 SDK internally fulfilled via NuGet
-        "http://some-url-pointing-to-winmd-file"
-        "http://some-url-pointing-to-zip-file-containing-winmd-files"
-        "https://www.nuget.org/api/v2/package/Win2D.uwp/1.24.0"
-        "https://www.nuget.org/api/v2/package/Microsoft.Windows.SDK.Contracts/10.0.18362.2005"
+        "os"
     modules
-        "kittens"
         "windows.storage"
         "windows.ui.composition"
 );
