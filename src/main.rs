fea430b9c4330296ccc5b8fbf9ae9c7d7d1b0281
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-09 16:25:52 -0800

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
-winrt::import!(
-    dependencies
-        "os"
-    modules
-        // "windows.storage"
-        // "windows.ui.composition"
-        // "windows.ui.xaml.controls"
-        "windows.foundation"
-        "windows.ui"
-);
+// winrt::import!(
+//     dependencies
+//         "os"
+//     modules
+//         // "windows.storage"
+//         // "windows.ui.composition"
+//         // "windows.ui.xaml.controls"
+//         "windows.foundation"
+//         "windows.ui"
+// );
 
-fn main() -> winrt::Result<()> {
-    use windows::ui::*;
-    let color = Colors::alice_blue()?;
+// fn main() -> winrt::Result<()> {
+//     use windows::ui::*;
+//     let color = Colors::alice_blue()?;
 
-    let s = format!("{:?}", color);
-    assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
+//     let s = format!("{:?}", color);
+//     assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
-    println!("woot!");
-    Ok(())
+//     println!("woot!");
+//     Ok(())
+// }
+
+fn main() {
+    let reader = winmd::Reader::from_os().unwrap();
+
+    let namespaces = reader.namespaces();
+
+    for ns in namespaces {
+        println!("namespace {}", ns.name());
+
+        for ns in ns.namespaces() {
+            println!("  namespace {}", ns.name());
+
+            for ns in ns.namespaces() {
+                println!("    namespace {}", ns.name());
+            }
+   
+        }
+    }
 }
