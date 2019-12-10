23284498f8f6d29f6c63b4caccb4cbf8d635b060
Kenny Kerr <kekerr@microsoft.com>
2019-12-10 12:53:50 -0800

    hacks

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,36 @@ winrt::import!(
     dependencies
         "os"
     modules
-        // "windows.storage"
-        // "windows.ui.composition"
-        // "windows.ui.xaml.controls"
         "windows.foundation"
-        "windows.ui"
+        "windows.ui.xaml.controls"
 );
 
-fn main() -> winrt::Result<()> {
-    use windows::ui::*;
-    let color = Colors::alice_blue()?;
+// fn main() -> winrt::Result<()> {
+//     use windows::ui::*;
+//     let color = Colors::alice_blue()?;
 
-    let s = format!("{:?}", color);
-    assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
+//     let s = format!("{:?}", color);
+//     assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
-    println!("woot!");
-    Ok(())
-}
+//     println!("woot!");
+//     Ok(())
+// }
 
-// fn main() {
-//     let reader = winmd::Reader::from_os().unwrap();
+// fn module_to_namespace<'a>(reader: &'a winmd::Reader, module: &str) -> &'a str {
+//     let namespaces = reader.namespaces();
+//     for segment in module.split('.') {
 
+//     }
+// }
+
+ fn main() {
+    //  let reader = winmd::Reader::from_os().unwrap();
+
+    // let ns = reader.find_namespace_lowercase("windows.ui.composition").unwrap();
+
+    // println!("{}", ns.full_name());
+
+ }
 //     let namespaces = reader.namespaces();
 
 //     for ns in namespaces.iter() {
