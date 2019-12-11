f6546c6fdcc004ceb982a78617f36c9c57f452d7
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-11 09:40:41 -0800

    test

--- src/main.rs
+++ src/main.rs
@@ -10,37 +10,32 @@ winrt::import!(
         "windows.storage"
 );
 
-// fn main() -> winrt::Result<()> {
-//     use windows::ui::*;
-//     let color = Colors::alice_blue()?;
 
-//     let s = format!("{:?}", color);
-//     assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
-//     println!("woot!");
-//     Ok(())
-// }
+fn main() -> winrt::Result<()> {
+    use windows::ui::*;
+    let color: Color = Default::default(); // Colors::alice_blue()?;
 
-// fn module_to_namespace<'a>(reader: &'a winmd::Reader, module: &str) -> &'a str {
-//     let namespaces = reader.namespaces();
-//     for segment in module.split('.') {
+    let s = format!("{:?}", color);
+    assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
+    println!("woot!");
+    Ok(())
+}
+
+//  fn main() {
+
+//     let reader = winmd::Reader::from_os().unwrap();
+
+//      let ns = reader.find_namespace_lowercase("windows.foundation").unwrap();
+
+//      println!("ns {}", ns.full_name());
+
+//     for t in ns.interfaces() {
+//         println!("type {}", t.name().unwrap());
 //     }
-// }
 
- fn main() {
-
-    let reader = winmd::Reader::from_os().unwrap();
-
-     let ns = reader.find_namespace_lowercase("windows.foundation").unwrap();
-
-     println!("ns {}", ns.full_name());
-
-    for t in ns.interfaces() {
-        println!("type {}", t.name().unwrap());
-    }
-
- }
+//  }
 //     let namespaces = reader.namespaces();
 
 //     for ns in namespaces.iter() {
