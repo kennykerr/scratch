2b1235fdd1090e99f6fac85952283613e3b0a175
Kenny Kerr <kekerr@microsoft.com>
2019-12-17 11:30:04 -0800

    demo

--- src/main.rs
+++ src/main.rs
@@ -12,45 +12,98 @@ winrt::import!(
 
 fn main() -> winrt::Result<()> {
     use windows::ui::*;
-    let color: Color = Default::default(); // Colors::alice_blue()?;
+    let color: Color = Colors::alice_blue()?;
 
     let s = format!("{:?}", color);
-    //assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
+    println!("{:?}", color);
+    assert!(s == "Color { a: 255, r: 240, g: 248, b: 255 }");
 
     println!("woot!");
     Ok(())
 }
 
-//  fn main() {
-
+// fn main() {
 //     let reader = winmd::Reader::from_os().unwrap();
+//     let t = reader.find_type("Windows.UI.Colors").unwrap();
 
-//      let ns = reader.find_namespace_lowercase("windows.foundation").unwrap();
+//     for attribute in t.attributes() {
+//         let (namespace, name) = attribute.name();
+//         println!("{}.{}", namespace, name);
 
-//      println!("ns {}", ns.full_name());
+//         if name == "StaticAttribute" {
 
-//     for t in ns.interfaces() {
-//         println!("type {}", t.name().unwrap());
-//     }
+//             // let t = attribute.arguments().iter().find_map(|(_, sig)| {
+//             //     if let winmd::ArgumentSig::Type(value) = sig {
+//             //         Some(value)
+//             //     } else
+//             //     {
+//             //         None
+//             //     }
+//             // });
 
-//  }
-//     let namespaces = reader.namespaces();
-
-//     for ns in namespaces.iter() {
-//         println!("namespace {}", ns.name());
-
-//         for ns in ns.namespaces() {
-//             println!("  namespace {}", ns.name());
-
-//             for ns in ns.namespaces() {
-//                 println!("    namespace {}", ns.name());
+//             for (name, sig) in attribute.arguments() {
+//                 if let winmd::ArgumentSig::Type(value) = sig {
+//                     println!("{}", value.name());
+//                 }
 //             }
 //         }
 //     }
 
-//     let ns = reader.find_namespace("Windows.UI.Composition").unwrap();
-//     println!("{}", ns.name());
+//     //  let iter = t.attributes().filter_map(|attribute| {
+//     //     let (namespace, name) = attribute.name();
 
-//     let t = reader.find_type("Windows.Foundation.IStringable").unwrap();
-//     println!("{}", t.name().unwrap());
+//     //     match namespace {
+//     //         "Windows.Foundation.Metadata" => {
+//     //             match name {
+//     //                 // "ActivatableAttribute" => {
+
+//     //                 // },
+//     //                 "StaticAttribute" => {
+//     //                     attribute.arguments().iter().find_map(|(_, sig)| {
+//     //                         if let winmd::ArgumentSig::Type(value) = sig {
+//     //                             Some(winmd::FactoryAttribute::Static(*value))
+//     //                         } else
+//     //                         {
+//     //                             None
+//     //                         }
+//     //                     })
+//     //                 },
+//     //                 // "ComposableAttribute" => {
+
+//     //                 // }
+//     //                 _ => None,
+//     //             }
+//     //         }
+//     //         _ => None,
+//     //     }
+//     //  });
+
+//     //      let ns = reader.find_namespace_lowercase("windows.foundation").unwrap();
+
+//     //      println!("ns {}", ns.full_name());
+
+//     //     for t in ns.interfaces() {
+//     //         println!("type {}", t.name().unwrap());
+//     //     }
+
+//     //  }
+//     //     let namespaces = reader.namespaces();
+
+//     //     for ns in namespaces.iter() {
+//     //         println!("namespace {}", ns.name());
+
+//     //         for ns in ns.namespaces() {
+//     //             println!("  namespace {}", ns.name());
+
+//     //             for ns in ns.namespaces() {
+//     //                 println!("    namespace {}", ns.name());
+//     //             }
+//     //         }
+//     //     }
+
+//     //     let ns = reader.find_namespace("Windows.UI.Composition").unwrap();
+//     //     println!("{}", ns.name());
+
+//     //     let t = reader.find_type("Windows.Foundation.IStringable").unwrap();
+//     //     println!("{}", t.name().unwrap());
 // }
