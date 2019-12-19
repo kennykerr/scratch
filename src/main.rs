7a0fc4a4e3e8e7b7d0371fcb1faa2cc103a71eea
Kenny Kerr <kekerr@microsoft.com>
2019-12-19 11:42:37 -0800

    guids

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-winrt::import!(
-    dependencies
-        "os"
-    modules
-        "windows.ui"
-);
+// winrt::import!(
+//     dependencies
+//         "os"
+//     modules
+//         "windows.ui"
+// );
 
-use windows::ui::*;
+// use windows::ui::*;
 
-fn main() -> winrt::Result<()> {
-    let color = Colors::red()?;
+// fn main() -> winrt::Result<()> {
+//     let color = Colors::red()?;
 
-    println!("{:?}", color);
+//     println!("{:?}", color);
 
-    println!("woot!");
-    Ok(())
+//     println!("woot!");
+//     Ok(())
+// }
+
+fn main() {
+    use winmd::*;
+
+    let reader = Reader::from_os().unwrap();
+    let t = reader.find_type("Windows.Foundation.IStringable").unwrap();
+    let a = t.find_attribute("Windows.Foundation.Metadata.GuidAttribute").unwrap();
+
+    for (_, sig) in a.arguments() {
+        match sig {
+            ArgumentSig::U8(value) => print!(" {:x}", value),
+            ArgumentSig::U16(value) => print!(" {:x}", value),
+            ArgumentSig::U32(value) => print!(" {:x}", value),
+            _ => panic!(),
+        }
+    }
 }
