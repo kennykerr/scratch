a65c71b605c0a58d4001eceb77fa0c65447de722
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-19 14:17:00 -0800

    guid

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
-// winrt::import!(
-//     dependencies
-//         "os"
-//     modules
-//         "windows.ui"
-// );
+winrt::import!(
+    dependencies
+        "os"
+    modules
+        "windows.ui"
+);
 
-// use windows::ui::*;
+use windows::ui::*;
 
-// fn main() -> winrt::Result<()> {
-//     let color = Colors::red()?;
+fn main() -> winrt::Result<()> {
+    let color = Colors::red()?;
 
-//     println!("{:?}", color);
+    println!("{:?}", color);
+    assert!(color == ColorHelper::from_argb(255, 255, 0, 0)?);
 
-//     println!("woot!");
-//     Ok(())
-// }
-
-fn main() {
-    use winmd::*;
-
-    let reader = Reader::from_os().unwrap();
-    let t = reader.find_type("Windows.Foundation.IStringable").unwrap();
-    let a = t.find_attribute("Windows.Foundation.Metadata.GuidAttribute").unwrap();
-
-    for (_, sig) in a.arguments() {
-        match sig {
-            ArgumentSig::U8(value) => print!(" {:x}", value),
-            ArgumentSig::U16(value) => print!(" {:x}", value),
-            ArgumentSig::U32(value) => print!(" {:x}", value),
-            _ => panic!(),
-        }
-    }
+    println!("woot!");
+    Ok(())
 }
