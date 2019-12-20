8b36c3479ced48edba3b5a146dea63340bd19ae1
Kenny Kerr <kenny.kerr@microsoft.com>
2019-12-20 11:45:23 -0800

    generics

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,33 @@ winrt::import!(
         "os"
     modules
         "windows.ui"
+        //"windows.foundation"
 );
 
 use windows::ui::*;
 
+fn test_reader() {
+    let reader = winmd::Reader::from_files(&[r"C:\Windows\System32\WinMetadata\Windows.Foundation.winmd".to_string()]).unwrap();
+    let t = reader.find_type("Windows.Foundation.IAsyncOperationWithProgress`2").unwrap();
+    let g = t.generics();
+
+    if g.is_empty() {
+        println!("{} is not generic", t.name());
+    } else {
+        for param in g {
+            print!("{}, ", param.name());
+        }
+    }
+}
+
 fn main() -> winrt::Result<()> {
-    let color = Colors::red()?;
+    test_reader();
 
-    println!("{:?}", color);
-    assert!(color == ColorHelper::from_argb(255, 255, 0, 0)?);
+    // let color = Colors::red()?;
 
-    println!("woot!");
+    // println!("{:?}", color);
+    // assert!(color == ColorHelper::from_argb(255, 255, 0, 0)?);
+
+    // println!("woot!");
     Ok(())
 }
