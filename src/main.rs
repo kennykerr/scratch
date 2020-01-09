f93609e85cca27e744b0d0d26640aaefbea96bcd
Kenny Kerr <kenny.kerr@microsoft.com>
2020-01-09 10:32:58 -0800

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,49 @@
+
+
+
 use winmd::*;
 
-// fn main() -> winrt::Result<()> {
-//     let mut writer = RustWriter::new();
-//     //writer.add_namespace("Windows.Foundation");
-//     //writer.add_namespace("Windows.UI.Composition");
-//     writer.add_namespace("Windows.UI");
-
-//     let tokens = writer.write();
-//     //println!("{}", tokens.to_string());
-
-//     Ok(())
-// }
-
-winrt::import!(
-    dependencies
-        "os"
-    modules
-        "windows.ui"
-        //"windows.foundation"
-);
-
- use windows::ui::*;
-
 fn main() -> winrt::Result<()> {
-    //test_reader();
+    let mut writer = RustWriter::new();
+    //writer.add_namespace("Windows.Foundation");
+    //writer.add_namespace("Windows.UI.Composition");
+    writer.add_namespace("Windows.UI");
 
-    let a = winrt::String::new();
-    assert!(a.is_empty());
-    assert!(a.len() == 0);
-    assert!(a.as_chars().len() == 0);
-
-    let hello = winrt::String::from("Hello");
-    assert!(!hello.is_empty());
-    assert!(hello.len() == 5);
-
-    let color = Colors::red()?;
-    println!("{:?}", color);
-    assert!(color == ColorHelper::from_argb(255, 255, 0, 0)?);
-    println!("woot!");
+    let tokens = writer.write();
+    //println!("{}", tokens.to_string());
 
     Ok(())
 }
+
+// winrt::import!(
+//     dependencies
+//         "os"
+//     modules
+//         "windows.ui"
+//         //"windows.foundation"
+// );
+
+//  use windows::ui::*;
+
+// fn main() -> winrt::Result<()> {
+//     //test_reader();
+
+//     let a = winrt::String::new();
+//     assert!(a.is_empty());
+//     assert!(a.len() == 0);
+//     assert!(a.as_chars().len() == 0);
+
+//     let hello = winrt::String::from("Hello");
+//     assert!(!hello.is_empty());
+//     assert!(hello.len() == 5);
+
+//     let color = Colors::red()?;
+//     println!("{:?}", color);
+//     assert!(color == ColorHelper::from_argb(255, 255, 0, 0)?);
+//     println!("woot!");
+
+//     Ok(())
+// }
 
 // // fn test_reader() {
 // //     let reader = winmd::Reader::from_files(&[
