740e33e9b61de35281a29fa4429a629aba313aa9
Kenny Kerr <kekerr@microsoft.com>
2020-01-21 08:22:08 -0800

    into

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,15 @@ modules
 fn main() -> Result<()> {
     use windows::foundation::*;
 
-    let uri = Uri::create_uri("http://kennykerr.ca")?;
+    let uri = &Uri::create_uri("http://kennykerr.ca")?;
+    println!("domain: {}", uri.domain()?);
+
+    let d : IUriRuntimeClass = uri.into();
+    println!("domain: {}", d.domain()?);
+    let s : IStringable = uri.into();
+    let s = s.to_string()?;
+    println!("stringable: {}", s);
+
     println!("domain: {}", uri.domain()?);
 
     Ok(())
