094854d230ad9c733a7f1d6ad511367eab03676a
Kenny Kerr <kekerr@microsoft.com>
2020-01-21 10:05:50 -0800

    AsRef

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,62 @@ use winrt::*;
 
 import!(
     dependencies
-    "os"
-modules
-    "windows.ui"
-    //"windows.foundation"
+        "os"
+    modules
+        "windows.foundation"
 );
 
+use windows::foundation::*;
+
+struct A{}
+struct B{}
+
+impl AsRef<B> for B {
+    fn as_ref(&self) -> &B {
+        self
+    }
+}
+
+impl AsRef<B> for A {
+    fn as_ref(&self) -> &B {
+        &B{}
+    }
+}
+
+fn call<T: AsRef<B>>(value: T) {
+    println!("call");
+}
+
+// fn call<T:AsRef<IStringable>>(s: T) -> Result<()> {
+//     println!("call {}", s.as_ref().to_string()?);
+
+//     Ok(())
+// }
+
 fn main() -> Result<()> {
-    use windows::foundation::*;
 
-    let uri = &Uri::create_uri("http://kennykerr.ca")?;
-    println!("domain: {}", uri.domain()?);
+    call(B{});
+    let b = B{};
+    call(&b);
+    call(A{});
+    let a = A{};
+    call(&a);
 
-    let d: IUriRuntimeClass = uri.into();
-    println!("domain: {}", d.domain()?);
+    // let uri = &Uri::create_uri("http://kennykerr.ca")?;
+    // println!("domain: {}", uri.domain()?);
+
+    // let d: IUriRuntimeClass = uri.into();
+    // println!("domain: {}", d.domain()?);
+    // println!("port: {}", d.port()?);
     
-    let s: IStringable = uri.into();
-    let s = s.to_string()?;
-    println!("stringable: {}", s);
+    // let s: IStringable = uri.into();
+    // let value = s.to_string()?;
+    // println!("stringable: {}", value);
 
-    println!("domain: {}", uri.domain()?);
+    // println!("domain: {}", uri.domain()?);
+
+    // call(&s);
+    // call(s);
 
     Ok(())
 }
