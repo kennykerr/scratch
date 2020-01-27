e3cffa53beca9990c97c7a1a323e6b1f3d64742e
Kenny Kerr <kenny.kerr@microsoft.com>
2020-01-26 20:00:05 -0800

    abi

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,16 @@ winrt::import!(
 
 use windows::foundation::collections::*;
 use windows::foundation::*;
-use winrt::QueryType;
+use winrt::*;
+
+// struct abi_IStringable
+//         {
+//             __0 : usize, __1 : usize, __2 : usize, __3 : usize, __4 : usize,
+//             __5 : usize, to_string : extern "system" fn
+//             (winrt :: RawPtr, * mut winrt :: RawPtr,) -> winrt :: ErrorCode,
+//         }
+
+type vcall = extern "system" fn (winrt :: RawPtr, * mut winrt :: RawPtr,) -> winrt :: ErrorCode;
 
 fn main() -> winrt::Result<()> {
     let uri = &Uri::create_uri("http://kennykerr.ca")?;
@@ -33,6 +42,18 @@ fn main() -> winrt::Result<()> {
     let s: IStringable = uri.into();
     let value = s.to_string()?;
     println!("stringable: {}", value);
+
+    // let o: Object = s.into();
+    // let tn = o.type_name()?;
+
+    // unsafe {
+    //     let abi = s.abi() as *mut *mut usize;
+    //     let abi = *abi;
+    //     let abi = abi.offset(7);
+    //     let abi = abi as *const vcall;
+    //     let hstring = winrt::String::new();
+    //     let hr = *abi(s.abi(), hstring.set_abi());
+    // }
 
     println!("domain: {}", uri.domain()?);
 
