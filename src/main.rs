7930f1d70a1de3cffe14079a449313eed2cb6ff1
Kenny Kerr <kekerr@microsoft.com>
2020-02-19 07:36:05 -0800

    clean

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,10 @@ winrt::import!(
         //"windows.ui.xaml"
 );
 
-//use windows::foundation::collections::*;
+use windows::foundation::collections::*;
 use windows::foundation::*;
 use windows::graphics::capture::*;
 use winrt::*;
-
-// // impl QueryType for IVectorView<IWwwFormUrlDecoderEntry> {
-
-// // }
-
-// // impl QueryType for IVectorView<IWwwFormUrlDecoderEntry> {
-// //     fn type_guid() -> &'static winrt::Guid {
-// //         static GUID: winrt::Guid = winrt::Guid::from_values(
-// //             // b1f00d3b-1f06-5117-93ea-2a0d79116701
-// //             0xb1f00d3b,
-// //             0x1f06,
-// //             0x5117,
-// //             &[0x93, 0xea, 0x2a, 0x0d, 0x79, 0x11, 0x67, 0x01],
-// //         );
-// //         &GUID
-// //     }
-// // }
-
-// fn c() -> Option<u32> {
-//     Some(123)
-// }
-
-// fn b() -> Option<u32> {
-//     let v = c()?;
-//     Some(v)
-// }
-
-// fn a() -> Option<u32> {
-//     let v = b()?;
-//     Some(v)
-// }
 
 fn main() -> winrt::Result<()> {
     println!("supported: {}", GraphicsCaptureSession::is_supported()?);
@@ -74,7 +43,7 @@ fn main() -> winrt::Result<()> {
     {
         let a = GuidHelper::create_new_guid()?;
         let b = GuidHelper::create_new_guid()?;
-        // TODO: the ABI for these aren't projecting correctly
+        // TODO: the ABI for these aren't projecting correctly - not sure why they don't AV
         assert!(!GuidHelper::equals(a, b)?);
         assert!(GuidHelper::equals(a, a)?);
     }
@@ -116,11 +85,9 @@ fn main() -> winrt::Result<()> {
         let decoder = uri.query_parsed()?;
         assert!(!decoder.is_empty());
 
-        //decoder.size();
-
         // TODO: need generic guids!
-        // let v: IVectorView<IWwwFormUrlDecoderEntry> = decoder.into();
-        // assert!(v.is_empty());
+        let v: IVectorView<IWwwFormUrlDecoderEntry> = decoder.into();
+        assert!(v.is_empty());
     }
 
     {
@@ -156,52 +123,5 @@ fn main() -> winrt::Result<()> {
         }
     }
 
-    //     {}
-
-    //     // //let o: Object = s.into();
-    //     // //let tn = o.type_name()?;
-
-    //     // unsafe {
-    //     //     type vcall = extern "system" fn(winrt::RawPtr, *mut winrt::RawPtr) -> winrt::ErrorCode;
-    //     //     let abi = s.abi() as *const *const vcall;
-    //     //     // Or the preceding two lines in one:
-    //     //     // let abi = s.abi() as *const *const extern "system" fn (winrt :: RawPtr, * mut winrt :: RawPtr,) -> winrt :: ErrorCode;
-    //     //     let abi = (*abi).offset(6);
-
-    //     //     let mut hstring = winrt::String::new();
-    //     //     let hr = (*abi)(s.abi(), hstring.set_abi());
-    //     //     println!("hr: {} string: {}", hr.0, hstring);
-
-    //     //     // let mut hstring = std::ptr::null_mut();
-    //     //     // let hr = (*abi)(s.abi(), &mut hstring);
-    //     //     // let hstring: winrt::String = std::mem::transmute(hstring);
-    //     //     // println!("hr: {} string: {}", hr.0, hstring);
-    //     // }
-
-    //     // println!("domain: {}", uri.domain()?);
-
-    //     // let _v = uri.query_parsed()?;
-
-    //     // call(uri)?;
-    //     // call(uri.clone())?;
-    //     // //call(d);
-    //     // call(&s)?;
-    //     // call(Uri::create_uri("http://kennykerr.ca")?)?;
-
-    //     // let o: winrt::Object = s.query();
-    //     // let s: IStringable = o.query();
-    //     // println!("o: {}", s.to_string()?);
-
-    //     // unsafe {
-    //     //     let v: IVector<i32> = std::mem::zeroed();
-    //     //     if false {
-    //     //         v.at(123);
-    //     //     }
-    //     // }
-
     Ok(())
 }
-
-// // // fn call<'a, T: Into<winrt::Param<'a, IStringable>>>(_s: T) -> winrt::Result<()> {
-// // //     Ok(())
-// // // }
