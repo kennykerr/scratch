29d5b23c970b170b12647262c92fb4ef3831701e
Kenny Kerr <kenny.kerr@microsoft.com>
2020-03-05 14:11:43 -0800

    macros

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-winrt::import!(
+use windows::foundation::*;
+use winrt::*;
+
+import!(
     dependencies:
         "os"
     modules:
         "windows.foundation.collections"
 );
 
-use winrt::*;
-use windows::foundation::*;
+// TODO:
+//  The class macro adds a pointer to a static vtbl for each interface.
+//  Implements the From trait to allow conversion to wrapped COM object
+// from any Stringable by moving it into the COM object.
+#[class(
+    implements(IStringable, IClosable),
+    type_name("Windows.Foundation.Stringable")
+)]
+struct Stringable {
+    value: String,
+}
+
+impl traits::IStringable for Stringable {
+    fn to_string(&self) -> Result<HString> {
+        Ok((&self.value).into())
+    }
+}
 
 fn main() -> Result<()> {
     let uri = Uri::create_uri("https://kennykerr.ca")?;
