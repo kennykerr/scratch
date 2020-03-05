8db63f8044d9f2bc2e2e6ced9fc619f8a839f080
Kenny Kerr <kenny.kerr@microsoft.com>
2020-03-05 08:38:40 -0800

    TypeGuid

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
-// winrt::import!(
-//     dependencies:
-//         "os"
-//     modules:
-//         "windows.foundation.collections"
-// );
+winrt::import!(
+    dependencies:
+        "os"
+    modules:
+        "windows.foundation.collections"
+);
 
-struct Thing {}
+use winrt::*;
+use windows::foundation::*;
 
-trait IStringable {
-    // This works fine:
-    fn to_string1(&self);
+fn main() -> Result<()> {
+    let uri = Uri::create_uri("https://kennykerr.ca")?;
+    println!("{}", uri.domain()?);
+    println!("{}", uri.to_string()?);
 
-    // This does not:
-    fn to_string2<'a, T: Into<Thing>>(&self, t: T);
-}
-
-struct Rc<T: ?Sized> {
-    placeholder: std::marker::PhantomData<T>,
-}
-
-fn call(thing: Rc<dyn IStringable>) {
-}
-
-fn main() {
+    Ok(())
 }
