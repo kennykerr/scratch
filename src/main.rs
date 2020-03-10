1319893a07df678795f17e92c7bb2f6e3bb4c930
Kenny Kerr <kenny.kerr@microsoft.com>
2020-03-10 15:54:40 -0700

    new

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,85 @@
-use windows::foundation::collections::*;
-use windows::foundation::*;
-use winrt::*;
+use winmd::*;
 
-use windows::foundation::traits as wf_traits;
-
-import!(
-    dependencies:
-        "os"
-    modules:
-        "windows.foundation.collections"
-);
-
-// TODO:
-//  The class macro adds a pointer to a static vtbl for each interface.
-//  Implements the From trait to allow conversion to wrapped COM object
-// from any Stringable by moving it into the COM object.
-#[class(
-    implements(IStringable, IClosable),
-    type_name("Windows.Foundation.Stringable")
-)]
-struct Stringable {
-    value: String,
+fn main() {
+    let file = File::new(r"c:\windows\system32\WinMetadata\Windows.Foundation.winmd");
+    let types = file.type_def_table();
+    println!("rows: {}", types.row_count);
 }
 
-impl wf_traits::IStringable for Stringable {
-    fn to_string(&self) -> Result<HString> {
-        Ok((&self.value).into())
-    }
-}
+// use windows::foundation::collections::*;
+// use windows::foundation::*;
+// use winrt::*;
 
-// impl TypeGuid for IVectorView<IWwwFormUrlDecoderEntry> {
-//     fn type_guid() -> &'static Guid {
-//         static GUID: Guid = Guid::from_values(
-//             0x00000035,
-//             0x0000,
-//             0x0000,
-//             &[0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46],
-//         );
-//         &GUID
+// use windows::foundation::traits as wf_traits;
+
+// import!(
+//     dependencies:
+//         "os"
+//     modules:
+//         "windows.foundation.collections"
+// );
+
+// // TODO:
+// //  The class macro adds a pointer to a static vtbl for each interface.
+// //  Implements the From trait to allow conversion to wrapped COM object
+// // from any Stringable by moving it into the COM object.
+// #[class(
+//     implements(IStringable, IClosable),
+//     type_name("Windows.Foundation.Stringable")
+// )]
+// struct Stringable {
+//     value: String,
+// }
+
+// impl wf_traits::IStringable for Stringable {
+//     fn to_string(&self) -> Result<HString> {
+//         Ok((&self.value).into())
 //     }
 // }
 
-use sha1::*;
+// // impl TypeGuid for IVectorView<IWwwFormUrlDecoderEntry> {
+// //     fn type_guid() -> &'static Guid {
+// //         static GUID: Guid = Guid::from_values(
+// //             0x00000035,
+// //             0x0000,
+// //             0x0000,
+// //             &[0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46],
+// //         );
+// //         &GUID
+// //     }
+// // }
 
-fn main() -> Result<()> {
-    let hash = Sha1::from("hello world").digest().bytes();
-    println!("{:?}", hash.get(..16));
+// use sha1::*;
 
+// fn main() -> Result<()> {
+//     let hash = Sha1::from("hello world").digest().bytes();
+//     println!("{:?}", hash.get(..16));
 
-    let uri = &Uri::create_uri("https://kennykerr.ca")?;
-    println!("{}", uri.domain()?);
-    println!("{}", uri.to_string()?);
+//     let uri = &Uri::create_uri("https://kennykerr.ca")?;
+//     println!("{}", uri.domain()?);
+//     println!("{}", uri.to_string()?);
 
-    let s: IStringable = uri.into();
-    println!("{}", s.to_string()?);
+//     let s: IStringable = uri.into();
+//     println!("{}", s.to_string()?);
 
-    let decoder = uri.query_parsed()?;
+//     let decoder = uri.query_parsed()?;
 
-    let o: Object = uri.into();
+//     let o: Object = uri.into();
 
-    //let size = decoder.size();
+//     //let size = decoder.size();
 
-    let g = <IStringable as winrt::TypeGuid>::type_guid();
+//     let g = <IStringable as winrt::TypeGuid>::type_guid();
 
-    Ok(())
-}
-
-// use winrt::*;
-
-// fn call<'a, T:Into<Param<'a, Guid>>>(value:T) {
-
+//     Ok(())
 // }
 
-// fn main(){
-//     let g : Guid = Default::default();
-//     call(g);
-// }
+// // use winrt::*;
+
+// // fn call<'a, T:Into<Param<'a, Guid>>>(value:T) {
+
+// // }
+
+// // fn main(){
+// //     let g : Guid = Default::default();
+// //     call(g);
+// // }
