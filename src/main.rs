c6a6889b4595ce579284ecb1b9efe5b7ef09acf7
Kenny Kerr <kenny.kerr@microsoft.com>
2020-03-31 16:45:29 -0700

    enum

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,26 @@ use winmd::*;
 fn main() {
     let mut reader = &Reader::from_os();
 
-    let mut limits: TypeLimits = Default::default();
-    limits.insert(reader, "windows.foundation");
-
-    let stage = TypeStage::from_limits(reader, &limits);
-
-    println!("count: {}", stage.0.len());
-
-    let tree = stage.to_tree();
-
-    println!("tree");
-
-    let stream = tree.to_stream();
-
-    println!("stream");
-
+    let def = reader.resolve(("Windows.Foundation", "AsyncStatus"));
+    let info = def.info(reader);
+    println!("{:#?}", info);
+    let stream = info.to_stream();
     println!("{}", stream.to_string());
+
+    // let mut limits: TypeLimits = Default::default();
+    // limits.insert(reader, "windows.foundation");
+
+    // let stage = TypeStage::from_limits(reader, &limits);
+
+    // println!("count: {}", stage.0.len());
+
+    // let tree = stage.to_tree();
+
+    // println!("tree");
+
+    // let stream = tree.to_stream();
+
+    // println!("stream");
+
+    // println!("{}", stream.to_string());
 }
