5916ed02835e324c38e74481c8807325b7e5a52d
Kenny Kerr <kenny.kerr@microsoft.com>
2020-04-02 20:28:42 -0700

    attributes

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
-winrt::import!(
-    dependencies:
-        "os"
-    modules:
-        "windows.foundation"
-);
+use proc_macro2::{Ident, Literal, TokenStream};
+use quote::{format_ident, quote};
+use std::collections::*;
+use std::iter::FromIterator;
+use winmd::*;
 
 fn main() {
-    let uri = Windows::Foundation::Uri {};
+    let reader = &Reader::from_os();
+
+    let def = reader.resolve(("Windows.Foundation", "IStringable"));
+    def.attribute(reader, ("Windows.Foundation.Metadata", "GuidAttribute")).arguments(reader);
+
+    let mut limits: TypeLimits = Default::default();
+    limits.insert(reader, "windows.foundation");
+
+    let stage = TypeStage::from_limits(reader, &limits);
+
+    println!("count: {}", stage.0.len());
+
+    let tree = stage.to_tree();
+
+    println!("tree");
+
+    let stream = tree.to_stream();
+
+    println!("stream");
+
+    println!("{}", stream.to_string());
 }
-
-
-// use proc_macro2::{Ident, Literal, TokenStream};
-// use quote::{format_ident, quote};
-// use std::collections::*;
-// use std::iter::FromIterator;
-// use winmd::*;
-
-// fn main() {
-//     let reader = &Reader::from_os();
-
-//     let mut limits: TypeLimits = Default::default();
-//     limits.insert(reader, "windows.foundation");
-
-//     let stage = TypeStage::from_limits(reader, &limits);
-
-//     println!("count: {}", stage.0.len());
-
-//     let tree = stage.to_tree();
-
-//     println!("tree");
-
-//     let stream = tree.to_stream();
-
-//     println!("stream");
-
-//     println!("{}", stream.to_string());
-// }
