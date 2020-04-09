204eaadef3a3ed035066494c58a51ccf02b1aa4a
Kenny Kerr <kenny.kerr@microsoft.com>
2020-04-08 21:39:04 -0700

    import test

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,29 @@
 //use quote::{format_ident, quote};
 //use std::collections::*;
 //use std::iter::FromIterator;
+//use winmd::*;
+
 use winmd::*;
+use winrt::*;
+
+// import!(
+//     dependencies
+//         "os"
+//     modules
+//         "windows.foundation"
+// );
+
+//use windows::foundation::*;
 
 fn main() {
-    let winmd_files = load_winmd::from_os();
-    let reader = &TypeReader::new(winmd_files);
+  //  let uri = Uri::default();
 
-    let def = reader.resolve(("Windows.Foundation", "WwwFormUrlDecoder"));
+     let winmd_files = load_winmd::from_os();
+     let reader = &TypeReader::new(winmd_files);
+     reader.resolve(("Windows.Foundation", "WwwFormUrlDecoder")).into_type(reader);
 
-    let t = def.into_type(reader);
 
-    println!("{:#?}", t);
-
-    // let mut limits: TypeLimits = Default::default();
+    // let mut limits = TypeLimits::default();
     // limits.insert(reader, "windows.foundation");
 
     // let stage = TypeStage::from_limits(reader, &limits);
