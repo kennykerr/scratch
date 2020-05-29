feeb4872f0c48571db1e9ac2e97d81b192c44ed5
Kenny Kerr <kenny.kerr@microsoft.com>
2020-05-29 12:54:19 -0700

    win2d

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+//use winrt::Builder;
+
 fn main() {
-    println!("cargo:rustc-env=KITTENS=CUTE");
+    // Builder::default()
+    //     .insert_nuget(&["TestComponent", "Win2D.uwp"])
+    //     .insert_namespaces(&["testcomponent", "microsoft.graphics.canvas"])
+    //     .build()
+    //     .unwrap();
 }
