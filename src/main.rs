281d129f607e7c892e6a0c7a1c99a81d3f115233
Kenny Kerr <kenny.kerr@microsoft.com>
2020-06-04 21:26:05 -0700

    samples

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,29 @@ use bindings::*;
 
 fn main() -> winrt::Result<()> {
     clipboard()?;
+    xml()?;
+
+    Ok(())
+}
+
+fn xml() -> winrt::Result<()> {
+    use windows::data::xml::dom::XmlDocument;
+    let doc = XmlDocument::new()?;
+
+    doc.load_xml(
+        "<things>
+            <color>red</color>
+            <animal>bird</animal>
+            <color>blue</color>
+        </things>",
+    )?;
+
+    let root = doc.document_element()?;
+    let colors = root.get_elements_by_tag_name("color")?;
+
+    for color in colors {
+        println!("{}", color.inner_text()?);
+    }
 
     Ok(())
 }
@@ -11,9 +34,11 @@ fn clipboard() -> winrt::Result<()> {
 
     let content = DataPackage::new()?;
     content.set_text("Rust/WinRT")?;
-     
+
     Clipboard::set_content(content)?;
     Clipboard::flush()?;
+
+    // "Rust/WinRT" is now on the clipboard
 
     Ok(())
 }
