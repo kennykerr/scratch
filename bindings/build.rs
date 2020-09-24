c885fa89cd8d85dd9d38cfdaae7d2cc6a0b49dee
Kenny Kerr <kenny.kerr@microsoft.com>
2020-09-24 11:42:21 -0700

    scratch

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,13 +1,6 @@
-winrt::build!(
-    dependencies
-        os
-    types
-        windows::application_model::data_transfer::*
-        windows::ui::composition::*
-        windows::data::xml::dom::*
-        windows::web::syndication::*
-);
-
 fn main() {
-    build();
+    winrt::build!(
+        types
+            windows::data::xml::dom::*
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,13 @@
-use bindings::windows::application_model::data_transfer::{Clipboard, DataPackage};
-use bindings::windows::data::xml::dom::XmlDocument;
-use bindings::windows::foundation::Uri;
-use bindings::windows::web::syndication::SyndicationClient;
+use bindings::windows::data::xml::dom::*;
 
 fn main() -> winrt::Result<()> {
-    clipboard()?;
-    xml()?;
-    feed()?;
-    Ok(())
-}
-
-fn clipboard() -> winrt::Result<()> {
-    let content = DataPackage::new()?;
-
-    Clipboard::set_content(content)?;
-    Clipboard::flush()?;
-
-    // "Rust/WinRT" is now on the clipboard
-
-    Ok(())
-}
-
-fn xml() -> winrt::Result<()> {
     let doc = XmlDocument::new()?;
-
-    doc.load_xml(
-        "<things>
-            <color>red</color>
-            <animal>bird</animal>
-            <color>blue</color>
-        </things>",
-    )?;
+    doc.load_xml("<html>hello world</html>")?;
 
     let root = doc.document_element()?;
-    let colors = root.get_elements_by_tag_name("color")?;
+    assert!(root.node_name()? == "html");
+    assert!(root.inner_text()? == "hello world");
 
-    for color in colors {
-        println!("color: {}", color.inner_text()?);
-    }
-
-    Ok(())
-}
-
-fn feed() -> winrt::Result<()> {
-    let uri = Uri::create_uri("https://kennykerr.ca/feed")?;
-
-    let client = SyndicationClient::new()?;
-
-    let feed = client.retrieve_feed_async(uri)?.get()?;
-    for item in feed.items()?.into_iter().take(3) {
-        println!("title: {}", item.title()?.text()?);
-    }
-
+    println!("done!");
     Ok(())
 }
